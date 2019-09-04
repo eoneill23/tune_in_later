703c40159f21d28fe42d9d8c3245e3197277ff55
Jon O'Drobinak <jodrobinak@gmail.com>
2019-09-03 18:19:23 -0600

    Add api call to get album data for given artist

--- src/Components/Apps/App.js
+++ src/Components/Apps/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }  from 'react';
 import './App.css';
+import { fetchAlbums } from '../../util/apiCalls'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      albums: []
+    }
+  }
+  componentDidMount() {
+    fetchAlbums()
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <section className="App">
+      
+      </section> 
+      );
+    }
 }
 
 export default App;
---
+++ src/util/apiCalls.js
@@ -0,0 +1,11 @@
+export const fetchAlbums = () => {
+  return fetch(`https://itunes.apple.com/search?term=R.E.M.&entity=album`)
+  .then(response => {
+    if(!response.ok) {
+      throw Error ('There was an issue retrieving your artist\'s albums. Please try again.')
+    }
+    return response.json()
+  })
+  .catch(error => {
+    throw Error ('There was an issue retrieving your artist\'s albums. Please try again.')})
+}
