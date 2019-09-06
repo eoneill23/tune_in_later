cf2d30875a04f1d30a0c59cc52bf78b891bfeac6
Jon O'Drobinak <jodrobinak@gmail.com>
2019-09-05 18:54:14 -0600

    Add loginForm, reactRouter, fetchUser

--- src/Components/Apps/App.js
+++ src/Components/Apps/App.js
@@ -1,15 +1,18 @@
 import React, { Component }  from 'react';
 import './App.css';
 import SearchForm from '../Forms/SearchForm'
-import Container from '../Container/Container'
-
+import Container from '../Container/Container';
+import { Router, Route, Link } from 'react-router-dom';
+import LogInForm from '../Forms/LogInForm';
+import SignUpForm from '../Forms/SignUpForm';
 
 class App extends Component {
   render() {
     return (
         <section className="App">
-          <SearchForm />
-          <Container/>
+          <Route exact path='/' render={() => <SearchForm /> && <Container/>} />
+          <Route exact path='/login' render={() => <LogInForm />} />
+          <Route exact path='/signup' render={() => <SignUpForm />} />
         </section>
       );
     }
--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -9,3 +9,22 @@ export const fetchAlbums = (queryArtist) => {
   .catch(error => {
     throw Error ('There was an issue retrieving your artist\'s albums. Please try again.')})
 }
+
+export const fetchUser = () => {
+  // const options = {
+  //   method: 'POST',
+  //   body: JSON.stringify({user}),
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // }
+  return fetch('https://localhost:3001/api/v1/login')
+    .then(response => {
+      if(!response.ok) {
+        throw Error ('There was an issue retrieving your account information. Please try again.')
+      }
+      return response.json()
+    })
+    .catch(error => {
+      throw Error ('There was an issue retrieving your account information. Please try again.')})
+}
