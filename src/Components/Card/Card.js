f9d9725db75c689a61640b7f602aa3b1b03f4c7e
Eric O'Neill <oneill.eric23@gmail.com>
2019-09-08 15:23:31 -0600

    Begin card details routing

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Card.css'
-import { addFavorite, invalidUser, removeFavoriteFromStore } from '../../actions/index'
+import { addFavorite, invalidUser, removeFavoriteFromStore } from '../../actions/index';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { postFavorite, deleteFavorite } from '../../util/apiCalls';
 
@@ -19,19 +20,16 @@ const Card = ({ album_id, artist_name, album_name, artwork_url, release_date, co
 			removeFavoriteFromStore(album_id);
 			deleteFavorite(album_id, user.id);
 		} else {
-			console.log("SHOULD GET THIS IF ISFAVORITE IS FALSE")
 			const albumData = {album_id, artist_name, album_name, artwork_url, release_date, content_advisory_rating, primary_genre_name}
 			postFavorite(albumData, user.id)
 			.then(favorite => addFavorite(favorite))
 			.catch(error => console.log(error))
-
 		}
-		isFavorite = !isFavorite;
 	}
 
  	return (
 		<article className={cardClassName}>
-			<img src={artwork_url} alt="Album cover art"/>
+				<Link to={`/cardDetails/${album_id}`}><img src={artwork_url} alt="Album cover art" /></Link>
 			<h2>{album_name}</h2>
 			<button onClick={(e) => isUserLoggedIn(e)}>Save For Later</button>
 		</article>
--- src/Components/CardDetails/CardDetails.js
+++ src/Components/CardDetails/CardDetails.js
@@ -1,17 +1,16 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom'
 
-// const CardDetails = ({ album_id, artist_name, album_name, artwork_url, release_date, content_advisory_rating, primary_genre_name, price, returnRoute}) => {
-//   console.log('HERE IS THE CARD', card)
-//   console.log('HERE IS THE RETURN ROUTE', returnRoute)
-//   return (
-//     <div>
-//       <Link to={`${returnRoute}`} className='back-btn'>◀ back</Link>
-//       <h1>{'google.com'}</h1>
-//       <img src={'google.com'} className='app-img-no-hover' />
-//       <p className='creature-bio'>{'google.com'}</p>
-//     </div>
-//   )
-// }
+const CardDetails = ({ album_id, artist_name, album_name, artwork_url, release_date, content_advisory_rating, primary_genre_name, price, returnRoute}) => {
+  console.log('HERE IS THE RETURN ROUTE', returnRoute)
+  return (
+    <div>
+      <Link to={`${returnRoute}`} className='back-btn'>◀ back</Link>
+      <h1>{'google.com'}</h1>
+      <img src={'google.com'} className='app-img-no-hover' />
+      <p className='creature-bio'>{'google.com'}</p>
+    </div>
+  )
+}
 
-// export default CardDetails;
+export default CardDetails;
