9e8338200b4ff5446bc1af38f030a1b79c1924b7
Jon O'Drobinak <jodrobinak@gmail.com>
2019-09-08 16:19:18 -0600

    Add additonal fields to card detail display

--- src/Components/CardDetails/CardDetails.js
+++ src/Components/CardDetails/CardDetails.js
@@ -7,8 +7,10 @@ const CardDetails = ({  artist_name, album_name, artwork_url, release_date,  pri
     <div>
       <Link to={`${returnRoute}`} className='back-btn'>◀ back</Link>
       <h1>{album_name}</h1>
+      <h1>{artist_name}</h1>
       <img src={artwork_url} className='app-img-no-hover' />
-      <p className='creature-bio'>{primary_genre_name}</p>
+      <p className='card-detail-genre'>{primary_genre_name}</p>
+      <p className='card-detail-release_date'>{release_date}</p>
     </div>
   )
 }
